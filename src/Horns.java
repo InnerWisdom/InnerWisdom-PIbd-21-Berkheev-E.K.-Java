3e43043692a1dd610f29d09e1a0cadacc8fdba8c
Emil InnerW <emil4uk273@gmail.com>
2020-10-18 22:41:33 +0400

    1

--- src/Horns.java
+++ src/Horns.java
@@ -14,52 +14,32 @@ public class Horns {
     }
 
     public void draw(Graphics g, Color color, int x, int y) {
-        switch (Objects.requireNonNull(count)) {
-            case one:
-                g.setColor(color);
-                g.drawLine(x + 5, y + 3, x - 14, y - 16);
-                g.drawLine(x - 14, y - 16, x + 5, y - 32);
 
-                g.drawLine(x + 5, y + 3, x + 24, y - 16);
-                g.drawLine(x + 24, y - 16, x + 5, y - 32);
-                break;
+        g.setColor(color);
+        g.drawLine(x + 5, y + 3, x - 14, y - 16);
+        g.drawLine(x - 14, y - 16, x + 5, y - 32);
 
+        g.drawLine(x + 5, y + 3, x + 24, y - 16);
+        g.drawLine(x + 24, y - 16, x + 5, y - 32);
 
-            case two:
-                g.setColor(color);
-                g.drawLine(x + 5, y + 3, x - 14, y - 16);
-                g.drawLine(x - 14, y - 16, x + 5, y - 32);
+        if(count ==CountOfHorns.two || count ==CountOfHorns.three){
 
-                g.drawLine(x + 5, y + 3, x + 24, y - 16);
-                g.drawLine(x + 24, y - 16, x + 5, y - 32);
+            g.drawLine(x + 5 + 50, y + 3, x - 14 + 50, y - 16);
+            g.drawLine(x - 14 + 50, y - 16, x + 5 + 50, y - 32);
 
-                g.drawLine(x + 5 + 50, y + 3, x - 14 + 50, y - 16);
-                g.drawLine(x - 14 + 50, y - 16, x + 5 + 50, y - 32);
+            g.drawLine(x + 5 + 50, y + 3, x + 24 + 50, y - 16);
+            g.drawLine(x + 24 + 50, y - 16, x + 5 + 50, y - 32);
 
-                g.drawLine(x + 5 + 50, y + 3, x + 24 + 50, y - 16);
-                g.drawLine(x + 24 + 50, y - 16, x + 5 + 50, y - 32);
-                break;
-            case three:
-                g.setColor(color);
-
-                g.drawLine(x + 5, y + 3, x - 14, y - 16);
-                g.drawLine(x - 14, y - 16, x + 5, y - 32);
-
-                g.drawLine(x + 5, y + 3, x + 24, y - 16);
-                g.drawLine(x + 24, y - 16, x + 5, y - 32);
-
-                g.drawLine(x + 5 + 50, y + 3, x - 14 + 50, y - 16);
-                g.drawLine(x - 14 + 50, y - 16, x + 5 + 50, y - 32);
-
-                g.drawLine(x + 5 + 50, y + 3, x + 24 + 50, y - 16);
-                g.drawLine(x + 24 + 50, y - 16, x + 5 + 50, y - 32);
+            if(count ==CountOfHorns.three){
 
                 g.drawLine(x + 5 + 100, y + 3, x - 14 + 100, y - 16);
                 g.drawLine(x - 14 + 100, y - 16, x + 5 + 100, y - 32);
 
                 g.drawLine(x + 5 + 100, y + 3, x + 24 + 100, y - 16);
                 g.drawLine(x + 24 + 100, y - 16, x + 5 + 100, y - 32);
-                break;
+            }
         }
+
+
     }
 }
