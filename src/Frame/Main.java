34d5612ea4369832fb18369e63a1b821a6dcc3de
Emil InnerW <emil4uk273@gmail.com>
2020-11-21 00:19:58 +0400

    fix-1

--- src/Frame/Main.java
+++ src/Frame/Main.java
@@ -3,8 +3,7 @@ package Frame;
 public class Main {
 
     public static void main(String[] args) {
-        FrameLocomotive frameLocomotive = new FrameLocomotive();
-        DrawPanel drawPanel = new DrawPanel();
-        frameLocomotive.addDrawPanel(drawPanel);
+        FrameDepo frameDepo = new FrameDepo();
+        frameDepo.init();
     }
 }
