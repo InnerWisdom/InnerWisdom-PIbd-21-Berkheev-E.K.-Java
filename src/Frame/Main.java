95d3a0df36a45536c29b72366dfd6fdf7b86ce5c
Emil InnerW <emil4uk273@gmail.com>
2020-12-13 01:14:39 +0400

    minor ones

--- src/Frame/Main.java
+++ src/Frame/Main.java
@@ -1,6 +1,6 @@
 package Frame;
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         FrameDepo frameDepo = new FrameDepo();
     }
 }
